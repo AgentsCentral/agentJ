80eac986f54ec533de5a4b2572fadd986d32eeb6
Rizwan Idrees <ridrees@gmail.com>
2025-08-03 19:57:28 +0100

    Adding User extractor interface

--- agentj-http/src/main/java/ai/agentscentral/http/request/UserExtractor.java
+++ agentj-http/src/main/java/ai/agentscentral/http/request/UserExtractor.java
@@ -3,7 +3,9 @@ package ai.agentscentral.http.request;
 import ai.agentscentral.core.session.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Optional;
+
 public interface UserExtractor {
 
-    User extract(HttpServletRequest request);
+    Optional<? extends User> extract(HttpServletRequest request);
 }
