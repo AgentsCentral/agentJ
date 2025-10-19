59adb6bd508382c68832202906e4ac7e69c63b85
Rizwan Idrees <ridrees@gmail.com>
2025-10-19 09:32:19 +0100

    adding interrupt

--- agentj-http/src/main/java/ai/agentscentral/http/request/Message.java
+++ agentj-http/src/main/java/ai/agentscentral/http/request/Message.java
@@ -1,12 +1,19 @@
 package ai.agentscentral.http.request;
 
 import ai.agentscentral.http.common.MessageType;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
  * Message
  *
  * @author Rizwan Idrees
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = TextMessage.class, name = "text"),
+        @JsonSubTypes.Type(value = InterruptMessage.class, name = "interrupt"),
+})
 public interface Message {
 
     MessageType type();
