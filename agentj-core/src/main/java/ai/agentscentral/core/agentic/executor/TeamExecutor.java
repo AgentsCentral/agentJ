ad3f434d128548897bccc3f7fb2e81bcad65a8ea
Rizwan Idrees <riz@usmobile.com>
2025-10-20 13:09:45 +0100

    handling interrupt pre-calls

--- agentj-core/src/main/java/ai/agentscentral/core/agentic/executor/TeamExecutor.java
+++ agentj-core/src/main/java/ai/agentscentral/core/agentic/executor/TeamExecutor.java
@@ -2,6 +2,11 @@ package ai.agentscentral.core.agentic.executor;
 
 import ai.agentscentral.core.team.Team;
 
+/**
+ * TeamExecutor
+ *
+ * @author Rizwan Idrees
+ */
 public interface TeamExecutor extends AgenticExecutor<Team> {
 
 }
