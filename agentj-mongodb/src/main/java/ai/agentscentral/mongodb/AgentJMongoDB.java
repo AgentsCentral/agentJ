1e34f4105c475a196b7e71d68962d3d3c964cd2f
mustafa-bhuiyan-dev <mustafa.devworld@gmail.com>
2025-12-02 22:10:03 -0500

    Feature #13: generalize the AgentJMongoDB generic methods and added upsert method

--- agentj-mongodb/src/main/java/ai/agentscentral/mongodb/AgentJMongoDB.java
+++ agentj-mongodb/src/main/java/ai/agentscentral/mongodb/AgentJMongoDB.java
@@ -2,12 +2,15 @@ package ai.agentscentral.mongodb;
 
 import ai.agentscentral.core.convertors.Convertor;
 import ai.agentscentral.mongodb.config.MongoDBConfig;
-import ai.agentscentral.mongodb.model.MessageDocument;
+import ai.agentscentral.mongodb.model.AgentJDocument;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.ReturnDocument;
 import org.bson.conversions.Bson;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -30,37 +33,51 @@ public class AgentJMongoDB {
         return database.getCollection(collectionName, documentClass);
     }
 
-    public <T> T findOne(MongoCollection<MessageDocument> collection,
-                               Bson filter,
-                               Bson sort, Convertor<MessageDocument, T> convertor) {
-        MessageDocument document = collection.find(filter).sort(sort).first();
+    public <T, D> T findOne(MongoCollection<D> collection,
+                               Bson filter, Convertor<D, T> convertor) {
+        D document = collection.find(filter).first();
         return Optional.ofNullable(document).map(convertor::convert).orElse(null);
     }
 
-    public <T> List<T> find(MongoCollection<MessageDocument> collection,
+    public <T, D> List<T> find(MongoCollection<D> collection,
                             Bson filter,
-                            Bson sort, Convertor<MessageDocument, T> convertor) {
-        FindIterable<MessageDocument> iterableDocs = collection.find(filter).sort(sort);
+                            Bson sort, Convertor<D, T> convertor) {
+        FindIterable<D> iterableDocs = collection.find(filter).sort(sort);
         return iterableDocs.map(convertor::convert).into(new ArrayList<>());
     }
 
-    public <T> void insertOne(MongoCollection<MessageDocument> collection, T record,
-                              Convertor<T, MessageDocument> convertor) {
+    public <T, D> void insertOne(MongoCollection<D> collection, T record,
+                              Convertor<T, D> convertor) {
 
         Optional.of(record)
                 .map(convertor::convert)
                 .map(collection::insertOne);
     }
 
-    public <T> void insertMany(MongoCollection<MessageDocument> collection,
+    public <T, D> void insertMany(MongoCollection<D> collection,
                                List<? extends T> records,
-                               Convertor<T, MessageDocument> convertor) {
+                               Convertor<T, D> convertor) {
 
 
-        final List<MessageDocument> documents = records.stream()
+        final List<D> documents = records.stream()
                 .map(convertor::convert).toList();
 
         collection.insertMany(documents);
     }
 
+    public <T, D extends AgentJDocument> D upsert(MongoCollection<D> collection, Bson filter, T record,
+                                                     Convertor<T, D> convertor) {
+        Instant now = Instant.now();
+
+        D document = convertor.convert(record);
+        document.setUpdatedAt(now);
+
+        boolean exists = collection.countDocuments(filter) > 0;
+        if (!exists) {
+            document.setCreatedAt(now);
+        }
+
+        return collection.findOneAndReplace(filter, document,
+                new FindOneAndReplaceOptions().upsert(true).returnDocument(ReturnDocument.AFTER));
+    }
 }
