4cade50d79f6be2e38d08cabfb0556c6542535ea
mustafa-bhuiyan-dev <mustafa.devworld@gmail.com>
2025-12-02 22:11:33 -0500

    Feature #13: added abstract general AgentJDocument to be extended to the concrete documents

---
+++ agentj-mongodb/src/main/java/ai/agentscentral/mongodb/model/AgentJDocument.java
@@ -0,0 +1,35 @@
+package ai.agentscentral.mongodb.model;
+
+import org.bson.types.ObjectId;
+
+import java.time.Instant;
+
+public abstract class AgentJDocument {
+    protected ObjectId id;
+    protected Instant createdAt;
+    protected Instant updatedAt;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
