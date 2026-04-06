349b851544e1b974088b32225a5239c7ef2a50da
Rizwan Idrees <ridrees@gmail.com>
2026-04-06 14:54:03 +0100

    updating http configs

--- agentj-http/src/main/java/ai/agentscentral/http/config/HttpConfig.java
+++ agentj-http/src/main/java/ai/agentscentral/http/config/HttpConfig.java
@@ -1,6 +1,5 @@
 package ai.agentscentral.http.config;
 
-import ai.agentscentral.core.agentic.Agentic;
 import ai.agentscentral.http.auth.Authorizer;
 import ai.agentscentral.http.config.cors.CORSConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,15 +9,11 @@ import java.util.List;
 /**
  * HttpConfig
  *
- * @param path
- * @param agentic
  * @param authorizers
  * @param corsConfig
  * @author Rizwan Idrees
  */
-public record HttpConfig(String path,
-                         Agentic agentic,
-                         List<Authorizer> authorizers,
+public record HttpConfig(List<Authorizer> authorizers,
                          CORSConfig corsConfig,
                          ObjectMapper objectMapper) {
 
